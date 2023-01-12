97f20347e68d57c693a1882a0346356119f1e5a5
dwiest <damian@wiest.world>
2023-01-12 22:28:49 +0000

    Initial commit

---
+++ django-selenium/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from selenium import webdriver
+from django.conf import settings
+
+
+class SeleniumForm(forms.Form):
+  url = forms.URLField(label='url',initial='')
+  width = forms.IntegerField(label='width',initial='1024',min_value=20, max_value=2048)
+  height = forms.IntegerField(label='height',initial='1024',min_value=20, max_value=3072)
+
+  def __init__(self, user, *args, **kwargs):
+    self.user = user
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--proxy-server=http://squid:3128")
+    self.options = options
+    self.browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=options)
+    super(forms.Form, self).__init__(*args, **kwargs)
+
+  def screenshot(self, *args, **kwargs):
+    self.browser.set_window_size(self.cleaned_data['width'],self.cleaned_data['height'])
+    self.browser.get(self.cleaned_data['url'])
+    self.browser.save_screenshot('static/' + str(self.user) + '-screenie.png')
+    
---
+++ django-selenium/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.template.context import RequestContext
+from django.views.generic import FormView
+from django.views.generic.base import TemplateResponseMixin
+from .forms import SeleniumForm
+
+class SeleniumView(FormView,TemplateResponseMixin):
+  template_name = "selenium.html"
+  success_url = '.'
+  form_class = SeleniumForm
+
+  def get(self, request, *args, **kwargs):
+    print("SeleniumView.get()")
+    form = SeleniumForm(request.user)
+    return render(request, self.template_name, {'form': form})
+
+  def post(self, request, *args, **kwargs):
+    print("SeleniumView.post()")
+    form = SeleniumForm(request.user, request.POST)
+    if form.is_valid():
+      form.screenshot()
+      return HttpResponseRedirect(request.path)
+    else:
+      return render(request, self.template_name, {'form': form})
