e476fc1938a45e384ec19c9ef0a65580d79c6fdb
dwiest <damian@wiest.world>
2023-01-16 20:15:52 +0000

    Set page_name and response_dict

--- dwiest/django/selenium/views.py
+++ dwiest/django/selenium/views.py
@@ -5,21 +5,32 @@ from django.views.generic import FormView
 from django.views.generic.base import TemplateResponseMixin
 from .forms import SeleniumForm
 
-class SeleniumView(FormView,TemplateResponseMixin):
+class SeleniumView(FormView, TemplateResponseMixin):
+  form_class = SeleniumForm
+  page_name = 'Selenium'
   template_name = "selenium/index.html"
   success_url = '.'
-  form_class = SeleniumForm
+
+  def __init__(self, *args, **kwargs):
+    self.response_dict = {
+      'page_name': self.page_name,
+    }
+
+    return super(FormView, self).__init__(*args, **kwargs)
+
 
   def get(self, request, *args, **kwargs):
     print("SeleniumView.get()")
     form = SeleniumForm(request.user)
-    return render(request, self.template_name, {'form': form})
+    self.response_dict['form'] = form
+    return render(request, self.template_name, self.response_dict)
 
   def post(self, request, *args, **kwargs):
     print("SeleniumView.post()")
     form = SeleniumForm(request.user, request.POST)
+    self.response_dict['form'] = form
     if form.is_valid():
       form.screenshot()
       return HttpResponseRedirect(request.path)
     else:
-      return render(request, self.template_name, {'form': form})
+      return render(request, self.template_name, self.response_dict)
